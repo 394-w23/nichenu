611f5349f3ff7bb7d866d5f336b68871fc2a760a
Adam Stewart <adamjoesph111@gmail.com>
2023-01-05 13:01:34 -0600

    adding firebase file and npm package

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDLkbW82yvxLiJt2Y1zKDwOIPLAfLrF6mo",
+  authDomain: "nichenu.firebaseapp.com",
+  projectId: "nichenu",
+  storageBucket: "nichenu.appspot.com",
+  messagingSenderId: "925606908543",
+  appId: "1:925606908543:web:ce09c97a243859d48fa54a"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
