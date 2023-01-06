fe1e04c4b26d8d69ac105ea12f9385196597e7b0
Adam Stewart <adamjoesph111@gmail.com>
2023-01-06 12:51:29 -0600

    adding bootstrap and radio buttons for options

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react';
 import './App.css';
 import './Header.jsx'
 import './Navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './Header.jsx';
 import Navbar from './Navbar.jsx';
 
 const App = () => {
   const [hobbies, setHobbies] = useState(0);
   const [events, setEvents] = useState(0);
+  const [currList,setCurrList] = useState(0);
+  const listOptions = ["events","hobbies","mystuff"]
 
   return (
     <div className="App">
       <Header />
-      <Navbar />
+      <Navbar listOptions={listOptions} selection={currList} setSelection={setCurrList} />
     </div>
   );
 };
--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,38 @@
 import "./Navbar.css";
 
-const Navbar = () => (
-  <div className="navbar">
-    <h1>Navbar</h1>
-  </div>
+
+const NavButton = ({ listOption, selection, setSelection }) => (
+    //console.log(listOption);
+    //console.log(selection);
+    //console.log(setSelection);
+
+    <div>
+        <input type="radio" id={listOption} className="btn-check" checked={listOption == selection} autoComplete="off"
+            onChange={() => setSelection(listOption)}/>
+        <label className="btn btn-outline-primary" htmlFor={listOption}>
+            {listOption}
+        </label>
+    </div>
 );
+
+const ListOptionSelector = ({listOptions, selection, setSelection} ) => {
+    //{console.log(listOptions)}
+    return (
+    <div className="btn-group">
+        {
+            listOptions.map(listOption => <NavButton key={listOption} listOption={listOption} selection={selection} setSelection={setSelection} />)
+        }
+    </div>
+    )
+};
+
+
+const Navbar = ( {listOptions, selection,setSelection} ) => {
+    return (
+        <div className="fixed-bottom text-center">
+            <ListOptionSelector listOptions={listOptions} selection={selection} setSelection={setSelection} />
+        </div>
+    );
+}
 
 export default Navbar;
