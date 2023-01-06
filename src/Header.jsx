f8d7da3f79f4082ffc587885fc464bd8edf5db25
Adam Stewart <adamjoesph111@gmail.com>
2023-01-06 12:51:42 -0600

    removing icon to make header look more centered

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,8 +2,9 @@ import "./Header.css";
 
 const Header = () => (
   <div className="header-top-row">
+    <div className="header-group">
     <h1 className="app-name">nicheNU</h1>
-    <img className="logo-image" src="../logo.svg" alt="nicheNU Logo"></img>
+    </div>
   </div>
 );
 
