ef005804b41fc9d6c2b0920c07b8747676d1ba3e
finnwintz5 <finn.wintz@gmail.com>
2023-01-09 19:14:24 -0600

    added onclick for CreateEvent, added user variable

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header.jsx'
 import Navbar from './components/Navbar.jsx'
 import HobbyList from './components/HobbyList';
 import EventList from './components/EventList';
+import CreateEvent from './components/CreateEvent';
 import ChatRoom from './components/ChatRoom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useDbData } from './utils/firebase';
@@ -13,6 +14,7 @@ const App = () => {
   const [data, error] = useDbData("/");
   const [currDisplay, setCurrDisplay] = useState("hobbies");
   const [hobby, setHobby] = useState("hobbies");
+  const [currUser, setCurrUser] = useState();
   const displayOptions = ["events","hobbies"];
 
   const openMessages= (hobby) => {
@@ -23,6 +25,7 @@ const App = () => {
   if (data === undefined) return <h1>Loading data...</h1>;
   if (!data) return <h1>No data found</h1>;
 
+
   return (
     <div className="App">
       <Header currDisplay={currDisplay} setCurrDisplay={setCurrDisplay}/>
@@ -30,7 +33,8 @@ const App = () => {
       currDisplay === "events" ? <EventList eventList={Object.values(data.events)}/> 
       : currDisplay === "hobbies" ? <HobbyList hobbyList={Object.values(data.hobbies)} openMessages={openMessages}/> 
       : currDisplay === "message" ? <ChatRoom hobby={hobby} users={Object.values(data.users)}/>
-      : <div></div>
+      : currDisplay === "createEvent" ? <CreateEvent user={currUser}/> :
+      <div></div>
       }
       {currDisplay !== 'message' && <Navbar displayOptions={displayOptions} selection={currDisplay} setSelection={setCurrDisplay} />}
     </div>
