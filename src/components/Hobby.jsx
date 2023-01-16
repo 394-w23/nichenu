a2b86631db28850ab72e852945985ed386fe1184
Adam Stewart <adamjoesph111@gmail.com>
2023-01-16 16:18:04 -0600

    adding default icon and making interactable space larger

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -1,18 +1,19 @@
 import './Hobby.css';
 import Tag from './Tag.jsx';
 import { useEffect, useState } from 'react';
+import { HiOutlineUserGroup } from "@react-icons/all-files/hi/HiOutlineUserGroup";
 
 const Hobby = ({ hobby, openMessages }) => {
     return (
-        <div className="hobby-card">
-            <img src={hobby.img}></img>
-            <div className="hobby-info" onClick={() => openMessages(hobby)}>
+        <div className="hobby-card" onClick={() => openMessages(hobby)}>
+            {hobby.img ? <img src={hobby.img}/> : <HiOutlineUserGroup className='hobby-img' size={70} />}
+            <div className="hobby-info" >
                 <div className="hobby-name">{hobby.name}</div>
                 <div>
                     {
                         hobby.tags
-                        ? Object.values(hobby.tags).map(tag => <Tag key={tag + hobby.id} tagName={tag} />)
-                        : <div></div>
+                            ? Object.values(hobby.tags).map(tag => <Tag key={tag + hobby.id} tagName={tag} />)
+                            : <div></div>
                     }
                 </div>
             </div>
