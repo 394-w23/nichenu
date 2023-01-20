4aa26950f8c13d90c1bffaad0b6ffb2e67dd8ce4
murirop <panashemuriro2023@u.northwestern.edu>
2023-01-20 11:59:29 -0600

    Add user to realtime db

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -2,15 +2,23 @@ import { Button, Text, Title } from '@mantine/core'
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
 import { RiGoogleFill } from "@react-icons/all-files/ri/RiGoogleFill"
-import { FirebaseSignIn, useAuth } from '../utils/firebase';
+import { FirebaseSignIn, useAuth, useDbData, useDbUpdate } from '../utils/firebase';
 
 export default function Auth({setCurrDisplay}) {
     // Sign in the user if they are not signed in already
     const user = useAuth();
-
+    const [data, error] = useDbData("/");
+    const [update, result] = useDbUpdate(`/users/${user? user.uid : "unknown"}`);
+   
+    
     useEffect(() => {
         console.log(user)
         if(user){
+            // if user is not in the raeltime database, add them.
+            let userData = {name: user.displayName, id: user.uid, profilePicture: user.photoURL, event_ids: [], hobbies_ids: [], }
+            if(!data.users[user.uid]){
+                update(userData)
+            }
         setCurrDisplay('hobbies')
         }
     },[user])
