982a44dec9335ee059a673c380768bf5033a64ce
murirop <panashemuriro2023@u.northwestern.edu>
2023-01-20 11:46:44 -0600

    If user is signed in, got to hobbies page

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,52 +1,30 @@
 import { Button, Text, Title } from '@mantine/core'
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
-import {RiGoogleFill} from "@react-icons/all-files/ri/RiGoogleFill"
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { RiGoogleFill } from "@react-icons/all-files/ri/RiGoogleFill"
+import { FirebaseSignIn, useAuth } from '../utils/firebase';
 
+export default function Auth({setCurrDisplay}) {
+    // Sign in the user if they are not signed in already
+    const user = useAuth();
 
-
-export default function Auth() {
-
-    const provider = new GoogleAuthProvider();
-
-
-const auth = getAuth();
-const signIn = async () => {
-    signInWithPopup(auth, provider)
-    .then((result) => {
-      // This gives you a Google Access Token. You can use it to access the Google API.
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential.accessToken;
-      // The signed-in user info.
-      const user = result.user;
-      console.log(user)
-      // ...
-    }).catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      // The email of the user's account used.
-      const email = error.customData.email;
-      // The AuthCredential type that was used.
-      const credential = GoogleAuthProvider.credentialFromError(error);
-      // ...
-    });
-}
-
-
-
-
-
-
+    useEffect(() => {
+        console.log(user)
+        if(user){
+        setCurrDisplay('hobbies')
+        }
+    },[user])
+    
+    
+  
     return (
-        <div style={{padding: 30, overflow: "hidden"}}>
+        <div style={{ padding: 30, overflow: "hidden" }}>
             <img src="/people.jpg" height="300" />
             <Text >Welcome to</Text>
             <Title>NicheNu</Title>
-            <Text style={{marginTop: 20}}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text>
-            <div style={{height: 70}}></div>
-            <Button onClick={signIn} leftIcon={<RiGoogleFill/>}>Signin with Google</Button>
+            <Text style={{ marginTop: 20 }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </Text>
+            <div style={{ height: 70 }}></div>
+            <Button onClick={FirebaseSignIn} leftIcon={<RiGoogleFill />}>Signin with Google</Button>
         </div>
     )
 }
