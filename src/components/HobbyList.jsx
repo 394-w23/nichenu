4192a7ca2d913c4017017856e27510296954415d
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-01-23 17:52:52 -0600

    Making sure authentication fully works by commenting out new stuff (new stuff to be implemented in new branch

--- src/components/HobbyList.jsx
+++ src/components/HobbyList.jsx
@@ -6,33 +6,33 @@ import { Title, Divider } from '@mantine/core'
 const HobbyList = ({ hobbyList, user, openMessages }) => {
     const [hobbies, setHobbies] = useState(hobbyList); 
 
-    let hasHobbies = true; //user.hobby_ids
+    // let hasHobbies = true; //user.hobby_ids
 
     console.log(hobbies)
 
     return (
-        // <div>
-        //     {
-        //         hobbies.map((hobby) => <Hobby key={hobby.id} hobby={hobby} openMessages={openMessages} />)
-        //     }
-        // </div>
         <div>
-            <br></br>
-            <Title order={2}>My Hobbies</Title>
             {
-                hasHobbies 
-                ? hobbies.map((hobby) => <Hobby key={hobby.id} hobby={hobby} openMessages={openMessages} />)
-                : <div className="empty-event-text">Go add events!</div> 
+                hobbies.map((hobby) => <Hobby key={hobby.id} hobby={hobby} openMessages={openMessages} />)
             }
-            <br></br>
-
-            {/* <Title order={2}>Other Hobbies</Title>
-            {
-                hasHobbies
-                ? events.filter((event) => !Object.values(event.users).includes(user.id)).sort((x, y) => (new Date(x.start_timestamp) - new Date(y.start_timestamp))).map((event) => <Event key={event.id} event={event} user={user} added={false} setCurrDisplay={setCurrDisplay} setEvents={setEvents} events={events} />)
-                : events.sort((x, y) => (new Date(x.start_timestamp) - new Date(y.start_timestamp))).map((event) => <Event key={event.id} event={event} user={user} added={false} setCurrDisplay={setCurrDisplay} setEvents={setEvents} events={events} />)
-            } */}
         </div>
+        // <div>
+        //     <br></br>
+        //     <Title order={2}>My Hobbies</Title>
+        //     {
+        //         hasHobbies 
+        //         ? hobbies.map((hobby) => <Hobby key={hobby.id} hobby={hobby} openMessages={openMessages} />)
+        //         : <div className="empty-event-text">Go add events!</div> 
+        //     }
+        //     <br></br>
+
+        //     {/* <Title order={2}>Other Hobbies</Title>
+        //     {
+        //         hasHobbies
+        //         ? events.filter((event) => !Object.values(event.users).includes(user.id)).sort((x, y) => (new Date(x.start_timestamp) - new Date(y.start_timestamp))).map((event) => <Event key={event.id} event={event} user={user} added={false} setCurrDisplay={setCurrDisplay} setEvents={setEvents} events={events} />)
+        //         : events.sort((x, y) => (new Date(x.start_timestamp) - new Date(y.start_timestamp))).map((event) => <Event key={event.id} event={event} user={user} added={false} setCurrDisplay={setCurrDisplay} setEvents={setEvents} events={events} />)
+        //     } */}
+        // </div>
     );
 }
 
