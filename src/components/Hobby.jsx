2f3135307c04da4d08a2f6d484301b3e3caf922a
Weff24 <jwny123@gmail.com>
2023-01-25 00:34:07 -0600

    Can only enter chatroom if hobby joined and fixed some hobby list and event list css

--- src/components/Hobby.jsx
+++ src/components/Hobby.jsx
@@ -2,16 +2,18 @@ import './Hobby.css';
 import Tag from './Tag.jsx';
 import { useEffect, useState } from 'react';
 import { HiOutlineUserGroup } from "@react-icons/all-files/hi/HiOutlineUserGroup";
-import { Button } from '@mantine/core';
+import { Button, Modal } from '@mantine/core';
 import { useDbData, useDbUpdate } from '../utils/firebase';
 
 const Hobby = ({ hobby, user, openMessages, added, setCurrDisplay, setHobbies, hobbies }) => {
     const [updateHobbyList, resultHobbyList] = useDbUpdate(`/hobbies/`); 
     const [updateHobby, resultHobby] = useDbUpdate(`/hobbies/${hobby.id}/message_chat/users/`);
     const [updateUser, resultUser] = useDbUpdate(`/users/${user.id}/hobby_ids/`);
+    const [modalOpened, setModalOpened] = useState(false);
 
     const JoinHobby = (e) => {
         e.preventDefault();
+        e.stopPropagation();
         // if (!e.target.value) return;
 
         updateHobby({
@@ -30,6 +32,7 @@ const Hobby = ({ hobby, user, openMessages, added, setCurrDisplay, setHobbies, h
 
     const LeaveHobby = (e) => {
         e.preventDefault();
+        e.stopPropagation();
         // if (!e.target.value) return;
         
         if (Object.values(hobby.message_chat.users).length == 1) {
@@ -53,34 +56,45 @@ const Hobby = ({ hobby, user, openMessages, added, setCurrDisplay, setHobbies, h
         setCurrDisplay("hobbies");
     }
 
+    const openChat = (e) => {
+        if (added) {
+            openMessages(hobby);
+        } else {
+            setModalOpened(true);
+        }
+    }
+
 
     return (
-        <div className="hobby-card" onClick={() => openMessages(hobby)}>
-            {hobby.img ? <img className="hobby-image" src={hobby.img}/> : 
-            // <HiOutlineUserGroup className='hobby-img' size={70} />
-            <img className="hobby-image" src="../../public/group.png" style={{width: "80%", padding: 10}}/>
-            }
-            <div className="hobby-info" >
-                <div className="hobby-name">{hobby.name}</div>
-                <div>
+        <div>
+            <Modal opened={modalOpened} onClose={() => setModalOpened(false)} withCloseButton={false} overlayColor={"#222"} overlayOpacity={0.45}>
+                Join the hobby to chat with its members!
+            </Modal>
+            <div className="hobby-card" onClick={openChat}>
+                {hobby.img ? <img className="hobby-image" src={hobby.img}/> : 
+                // <HiOutlineUserGroup className='hobby-img' size={70} />
+                <img className="hobby-image" src="../../public/group.png" style={{width: "80%", padding: 15}}/>
+                }
+                <div className="hobby-info" >
+                    <div className="hobby-name">{hobby.name}</div>
+                    <div>
+                        {
+                            hobby.tags
+                                ? Object.values(hobby.tags).map(tag => <Tag key={tag + hobby.id} tagName={tag} />)
+                                : <div></div>
+                        }
+                    </div>
+                </div>
+                <div className="hobbylist-button">
                     {
-                        hobby.tags
-                            ? Object.values(hobby.tags).map(tag => <Tag key={tag + hobby.id} tagName={tag} />)
-                            : <div></div>
+                        added
+                        ? <Button onClick={LeaveHobby} style={{marginLeft: 5}} size="xs" color="red">Leave</Button>
+                        : <Button onClick={JoinHobby} style={{marginLeft: 5}} size="xs">Join</Button>
                     }
                 </div>
             </div>
-            <div>
-                {
-                    added
-                    ? <Button onClick={LeaveHobby} style={{marginLeft: 5}} size="xs" color="red">Leave</Button>
-                    : <Button onClick={JoinHobby} style={{marginLeft: 5}} size="xs">Join</Button>
-                }
-            </div>
-            
-            
-
         </div>
+        
     );
 }
 
