df789b9cd41096dbf71119159b1a6dfa24bc9420
finnwintz5 <102768648+finnwintz5@users.noreply.github.com>
2023-01-25 19:41:42 -0600

    Formatting of header + logout button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,12 +16,16 @@ const Header = ({ currDisplay, setCurrDisplay }) => {
   return (<div style={{display: currDisplay == "auth"? "none": "flex"}} className="header-top-row">
     <div className="header-group">
       {currDisplay === "message" ?
-        <ActionIcon className="back-button" onClick={() => goBack()}>
-          <RiArrowLeftSLine className="back-arrow" size={32} />
-        </ActionIcon> : <div></div>}
+        <div style={{width: "24%"}}><ActionIcon className="back-button" onClick={() => goBack()}>
+        <RiArrowLeftSLine className="back-arrow" size={32} />
+      </ActionIcon></div> : <div style={{width: "25%"}}></div>}
+      {/* <div style={{width: currDisplay === "message"? 0: 80}}></div>     */}
       <h1 className="app-name">nicheNU</h1>      
-      <div style={{width: currDisplay === "message"? 30: 0}}></div>    
-    
+      {/* <div style={{width: currDisplay === "message"? 20: 0}}></div>     */}
+      <div style={{width: "25%",display: "flex", justifyContent: "right"}}>
+        <Button size="xs">Logout</Button>
+      </div>
+      
     </div>
   </div>);
 }
