dc00c7a9e9cd3cffb92174fa170c0c730eadefc5
Weff24 <jwny123@gmail.com>
2023-01-25 19:38:15 -0600

    Fixed last person to leave hobby bug

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -39,10 +39,11 @@ const ChatRoom = ({ hobby, users, user, setCurrDisplay }) => {
         // if (!e.target.value) return;
         
         if (Object.values(hobby.message_chat.users).length == 1) {
-            setHobbies(hobbies.filter((h) => h.id != hobby.id));
-
-            
             updateHobbyList({
+                [hobby.id]: null,
+            });
+
+            updateUser({
                 [hobby.id]: null,
             });
 
