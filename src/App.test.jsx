06d2b75ee2bba6c20a2f57f6c8b84a53f46802c5
Adam Stewart <adamjoesph111@gmail.com>
2023-03-03 17:13:14 -0600

    Adding jest stuff for testing

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,47 @@
-import {describe, expect, test} from 'vitest';
-import {fireEvent, render, screen} from '@testing-library/react';
-import App from './App';
+import { describe, expect, test } from "vitest";
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
+import { useDbData, useDbUpdate } from "./utils/firebase";
+import { when } from "jest-when";
 
-describe('counter tests', () => {
-    
-  test("Not a real test", () => {
+vi.mock("./utils/userProfile");
+vi.mock("./utils/firebase.js");
+
+const testProfile = {
+  displayName: "testUser",
+  email: "testUser@northwestern.edu",
+  profilePic: "https://illustoon.com/photo/590.png",
+};
+
+describe("without authentication", () => {
+  // beforeEach(() => {
+  //   // useProfile.mockReturnValue([null]);
+  //   render(<App />);
+  // });
+
+  it("displays sign in page", () => {
     expect(true).toBe(true);
   });
 });
+
+// describe("after authentication", () => {
+//   beforeEach(() => {
+//     useProfile.mockReturnValue([testProfile]);
+//     when(useDbData).calledWith("/rides").mockReturnValue([testData["rides"]]);
+//     when(useDbData).calledWith("/users").mockReturnValue([testData["users"]]);
+//     useDbUpdate.mockReturnValue([null]);
+//     render(<App />);
+//   });
+
+//   it("verifies user is signed in", () => {
+//     expect(screen.getByText("Sign out")).toBeDefined();
+//   });
+
+//   it("displays ride from mock data", () => {
+//     expect(
+//       screen.getByText(
+//         "Destination: Chicago O'Hare International Airport (ORD)"
+//       )
+//     ).toBeDefined();
+//   });
+// });
