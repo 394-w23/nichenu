3f5f0c8fe6e8474d3817cbcd8ebd138a7639aed5
Adam Stewart <adamjoesph111@gmail.com>
2023-03-10 17:01:47 -0600

    adding code coverage to things

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   test: {
+    coverage: {
+      provider: 'istanbul' // or 'c8'
+    },
     globals: true,
     environment: 'jsdom',
   }
